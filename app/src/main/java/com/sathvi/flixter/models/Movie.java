afaf5c5150be7c88617b12d3cf11553a1b6755d4
sathvika-shakhamuri <sathvi.sony@gmail.com>
2020-09-21 21:38:14 -0700

    Added flixter files

---
+++ app/src/main/java/com/sathvi/flixter/models/Movie.java
@@ -0,0 +1,41 @@
+package com.sathvi.flixter.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String title;
+    String overView;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overView = jsonObject.getString("overview");
+
+    }
+
+    public static List<Movie> fromJSONArray(JSONArray movieJsonArray) throws JSONException{
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i <movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverView() {
+        return overView;
+    }
+}
