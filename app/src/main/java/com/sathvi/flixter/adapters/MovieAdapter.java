92d8b25c69f39015e8355cc06793e070a99dc0a1
sathvika-shakhamuri <sathvi.sony@gmail.com>
2020-10-02 15:26:26 -0700

    - added Youtube View

--- app/src/main/java/com/sathvi/flixter/adapters/MovieAdapter.java
+++ app/src/main/java/com/sathvi/flixter/adapters/MovieAdapter.java
@@ -1,20 +1,27 @@
 package com.sathvi.flixter.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.sathvi.flixter.DetailActivity;
 import com.sathvi.flixter.R;
 import com.sathvi.flixter.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -53,18 +60,20 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster; //
-
+        RelativeLayout container;
 
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
             tvTitle =  itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster =  itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
 
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverView());
             String imageUrl;
@@ -77,6 +86,18 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             }
             // else default - poster path
             Glide.with(context).load(imageUrl).into(ivPoster);
+            //Register click listener on the whole row
+
+            container.setOnClickListener(new OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    // Navigate a new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
